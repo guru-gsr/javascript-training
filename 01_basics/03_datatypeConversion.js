c4c90b3879153fe5e4d301d6b91313997692aa8a
guru-gsr <159871956+guru-gsr@users.noreply.github.com>
2024-02-13 09:49:35 +0000

    learned about datatypes and their conversions

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,13 @@
+let a="1";
+let isNumber=Number(a)    //converting string to number
+console.log(typeof isNumber)   
+
+
+let num=10;
+let converToString=String(num)  // converting number to string
+console.log(typeof converToString)
+
+
+let x=1;
+let converToBoolean=Boolean(x)
+console.log(typeof converToBoolean);  // converting to Boolean
