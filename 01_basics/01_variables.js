6f17c6ac1317aac396fced842b68c53046bc8330
guru-gsr <159871956+guru-gsr@users.noreply.github.com>
2024-02-13 07:48:36 +0000

    updated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const employeeId=1223  //value of const variable declaration can't be change
+
+let employeeName="Gurusharan"    // mostly we use let to declare variables b/c of scope 
+
+var employeeEmail="gsr@gmail.com"   // we doesn,t use var because of issue in block scope and functional scope  
+
+employeeCity="Indore"
+
+let accountState;
+
+
+
+console.log(employeeId)
+console.log(employeeEmail)
+console.log(employeeName)
+console.log(employeeCity)
+console.log(accountState)
+// console.table([employeeId, employeeName,employeeEmail, employeeCity])   // to display in table form 
