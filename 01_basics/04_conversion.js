1ddcf83ab76923275237bbac1d1070505a0b49e9
guru-gsr <159871956+guru-gsr@users.noreply.github.com>
2024-02-13 10:21:17 +0000

    comaprision and equlaity operators

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,27 @@
+// console.log(2 >1);
+// console.log(2 < 1);
+// console.log(2 >=1);
+// console.log(2 ==1);
+// console.log(2 !=1);
+
+
+
+// comparision operators convert into the same type and then check
+// console.log("2 " > 1);
+// console.log("02" > 1);
+
+
+
+// equality ==  cannot convert null to a number while 
+// comparision > < >= <= converts null to a number and then check
+// console.log(null >0);
+// console.log(null ==0);  
+// console.log(null >=0);
+
+
+
+// strict check ===
+//=== cannot convert and check the datatypes of both the values which is going to be compare
+
+console.log("gsr " === 2);
+console.log(2 === 2);
