9996926f2183f0fa164ae273374d6579c00588a5
guru-gsr <gurusharansingh.rajput@mindpathtech.com>
2024-02-21 18:25:19 +0530

    change express

--- backendDev/src/index.js
+++ backendDev/src/index.js
@@ -1,7 +1,7 @@
 require ('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 4000
+//const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,6 +15,6 @@ app.get('/login',(req,res)=>{
   res.send('<h1>plaease login at my app</h1>')
 })
  
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
